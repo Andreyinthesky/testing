d9b63364e5c528ada6bb890c9f26fd83d9ee8dc3
Andrew <a.a.belousov@uspu.su>
2018-10-30 17:52:36 +0500

    Update NumberValidatorTests.cs added new test cases, test are divided, FA applied

--- cs/HomeExercises/NumberValidatorTests.cs
+++ cs/HomeExercises/NumberValidatorTests.cs
@@ -9,85 +9,74 @@ namespace HomeExercises
 	public class NumberValidatorTests
 	{
 		[Test]
-		[TestCaseSource(nameof(NumberValidatorTestCases_StringValue_ShouldBeMatchRegex), Category =
-			"StringValue_ShouldBeMatchRegex")]
-		[TestCaseSource(nameof(NumberValidatorTestCases_StringValue_ShouldBeNotNullOrEmpty), Category =
-			"StringValue_ShouldBeNotNullOrEmpty")]
-		[TestCaseSource(nameof(NumberValidatorTestCases_IntAndFracPartCountSigns), Category =
-			"IntAndFracPartCountSigns")]
-		[TestCaseSource(nameof(NumberValidatorTestCases_FracPartCountSigns), Category = "FracPartCountSigns")]
-		[TestCaseSource(nameof(NumberValidatorTestCases_NumberSign), Category = "NumberSign")]
-		public bool Test(int precision, int scale, bool onlyPositive, string value)
+		[TestCase(17, 2, true, "0.000", ExpectedResult = false, TestName = "when scale value more than scale")]
+		[TestCase(3, 2, true, "00.00", ExpectedResult = false, TestName = "when length value more than precision")]
+		[TestCase(3, 2, true, "-5.5", ExpectedResult = false, TestName = "when value have minus and onlyPositive true")]
+		[TestCase(5, 3, false, "--5.5", ExpectedResult = false, TestName = "when value have more then one sign")]
+		[TestCase(3, 2, false, "5.5-", ExpectedResult = false, TestName = "when value have sign in the end")]
+		[TestCase(3, 2, false, "5.-5", ExpectedResult = false, TestName = "when value have sign after decimal point")]
+		[TestCase(5, 3, false, "5.5.5", ExpectedResult = false,
+			TestName = "when value have more then one decimal point")]
+		[TestCase(3, 2, false, "+1.23", ExpectedResult = false,
+			TestName = "when length value with plus more than precision")]
+		[TestCase(3, 2, false, "-0.00", ExpectedResult = false,
+			TestName = "when length value with minus more than precision")]
+		[TestCase(3, 2, false, "", ExpectedResult = false, TestName = "when value is empty string")]
+		[TestCase(3, 2, false, null, ExpectedResult = false, TestName = "when value is null")]
+		[TestCase(3, 2, false, "5.s", ExpectedResult = false, TestName = "when value have letters after decimal point")]
+		[TestCase(3, 2, false, "5.", ExpectedResult = false, TestName = "when value have nothing after decimal point")]
+		[TestCase(3, 2, false, ".02", ExpectedResult = false,
+			TestName = "when value have nothing before decimal point")]
+		[TestCase(17, 2, true, "something", ExpectedResult = false, TestName = "when value have letters only")]
+		[TestCase(3, 2, true, "a.02", ExpectedResult = false,
+			TestName = "when value have letters before decimal point")]
+		[TestCase(3, 2, true, "a.sd", ExpectedResult = false,
+			TestName = "when value have letters before and after decimal point")]
+		[TestCase(4, 2, false, "-a.02", ExpectedResult = false,
+			TestName = "when value with minus and have letters after decimal point")]
+		[TestCase(3, 2, false, "+", ExpectedResult = false, TestName = "when value is plus")]
+		[TestCase(3, 2, false, "-", ExpectedResult = false, TestName = "when value is minus")]
+		[TestCase(3, 2, false, ",", ExpectedResult = false, TestName = "when value is comma")]
+		[TestCase(3, 2, false, ".", ExpectedResult = false, TestName = "when value is decimal point")]
+		public bool IsValidNumber_NotValidateNumber(int precision, int scale, bool onlyPositive, string value)
 		{
 			return new NumberValidator(precision, scale, onlyPositive).IsValidNumber(value);
 		}
 
-		public static IEnumerable NumberValidatorTestCases_StringValue_ShouldBeMatchRegex
+		[Test]
+		[TestCase(3, 2, false, "+0,0", ExpectedResult = true, TestName = "when value have plus and separated by comma")]
+		[TestCase(3, 2, false, "-0,0", ExpectedResult = true,
+			TestName = "when value have minus and separated by comma")]
+		[TestCase(3, 2, false, "0,0", ExpectedResult = true, TestName = "when value separated by comma")]
+		[TestCase(3, 2, false, "-0.0", ExpectedResult = true, TestName = "when separated by decimal point")]
+		[TestCase(17, 2, true, "0", ExpectedResult = true, TestName = "when value without scale")]
+		[TestCase(17, 2, true, "0.0", ExpectedResult = true, TestName = "when value length less than precision")]
+		[TestCase(3, 2, false, "+5.5", ExpectedResult = true, TestName = "when value have plus")]
+		[TestCase(3, 2, false, "-5.5", ExpectedResult = true, TestName = "when value have minus")]
+		public bool IsValidNumber_ValidateNumber(int precision, int scale, bool onlyPositive, string value)
 		{
-			get
-			{
-				yield return new TestCaseData(17, 2, true, "something").Returns(false);
-				yield return new TestCaseData(3, 2, true, "a.sd").Returns(false);
-				yield return new TestCaseData(3, 2, true, "0.sd").Returns(false);
-				yield return new TestCaseData(3, 2, true, "a.02").Returns(false);
-				yield return new TestCaseData(4, 2, false, "-a.02").Returns(false);
-			}
+			return new NumberValidator(precision, scale, onlyPositive).IsValidNumber(value);
 		}
 
-		public static IEnumerable NumberValidatorTestCases_StringValue_ShouldBeNotNullOrEmpty
+		[Test]
+		[TestCase(-1, 2, true, TestName = "when precision is not positive number")]
+		[TestCase(0, 2, true, TestName = "when precision is zero")]
+		[TestCase(2, 3, true, TestName = "when scale greater than precision")]
+		[TestCase(2, 2, true, TestName = "when scale equal precision")]
+		[TestCase(2, -1, true, TestName = "when scale is negative number")]
+		public void Constructor_ThrowArgumentException(int precision, int scale, bool onlyPositive)
 		{
-			get
-			{
-				yield return new TestCaseData(17, 2, true, null).Returns(false);
-				yield return new TestCaseData(17, 2, true, string.Empty).Returns(false);
-			}
+			Action act = () => new NumberValidator(precision, scale, onlyPositive);
+			act.ShouldThrow<ArgumentException>();
 		}
 
-		public static IEnumerable NumberValidatorTestCases_IntAndFracPartCountSigns
+		[Test]
+		[TestCase(1, 0, true, TestName = "when scale is zero")]
+		[TestCase(3, 2, true, TestName = "when scale is less than precision")]
+		public void Constructor_DoesNotThrowArgumentException(int precision, int scale, bool onlyPositive)
 		{
-			get
-			{
-				yield return new TestCaseData(17, 2, true, "0.0").Returns(true);
-				yield return new TestCaseData(17, 2, true, "0").Returns(true);
-				yield return new TestCaseData(4, 2, true, "+1.23").Returns(true);
-				yield return new TestCaseData(4, 2, true, "11.23").Returns(true);
-
-				yield return new TestCaseData(3, 2, true, "00.00").Returns(false);
-				yield return new TestCaseData(3, 2, false, "-0.00").Returns(false);
-			}
-		}
-
-		public static IEnumerable NumberValidatorTestCases_FracPartCountSigns
-		{
-			get { yield return new TestCaseData(17, 2, false, "0.000").Returns(false); }
-		}
-
-		public static IEnumerable NumberValidatorTestCases_NumberSign
-		{
-			get
-			{
-				yield return new TestCaseData(4, 2, true, "-0.00").Returns(false);
-				yield return new TestCaseData(4, 2, false, "-2").Returns(true);
-				yield return new TestCaseData(4, 2, false, "+2").Returns(true);
-				yield return new TestCaseData(4, 2, false, "2").Returns(true);
-			}
-		}
-
-		[Category("TestThrows")]
-		[TestCase(-1, 2, true, true)]
-		[TestCase(2, 2, true, true)]
-		[TestCase(1, 0, true, false)]
-		public void TestThrows(int precision, int scale, bool onlyPositive, bool doesThrow)
-		{
-			if (doesThrow)
-			{
-				Assert.That(() => new NumberValidator(precision, scale, onlyPositive),
-					Throws.TypeOf<ArgumentException>());
-			}
-			else
-			{
-				Assert.That(() => new NumberValidator(precision, scale, onlyPositive), Throws.Nothing);
-			}
+			Action act = () => new NumberValidator(precision, scale, onlyPositive);
+			act.ShouldNotThrow<ArgumentException>();
 		}
     }
 
